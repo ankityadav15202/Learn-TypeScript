ffbc6421111fcbf079ce52d7cc57987340e447c7
ankit <ankityadav15202@gmail.com>
2024-10-15 12:42:12 +0530

    Variable and Type inference

---
+++ 01intro/intro.ts
@@ -0,0 +1,7 @@
+let user = {name : "ankit", age : 15}
+
+console.log("ankit")
+console.log(user.name);
+
+
+export {}
---
+++ 01intro/variable.ts
@@ -0,0 +1,31 @@
+let greetings : string = "Hello ankit"
+greetings.toLowerCase()
+console.log(greetings);
+let mynum : number = 6
+
+
+let person :  {name : string, age : number} = {name : "ankit", age : 21} 
+console.log(person);
+
+
+//number
+
+let userId : number = 334466.545
+
+// boolean 
+
+let isLoggedin : boolean = false
+
+// any
+
+let hero : string;
+
+function getHero(){
+    return "thor"
+}
+
+
+hero = getHero()
+export {}
+
+// Type inference
